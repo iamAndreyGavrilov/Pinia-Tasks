1fae76b8507a1f5a0a421517d3548995f1a62ba6
Andrey G <a.gavrilov@promo-bot.ru>
2022-11-06 13:36:40 +0500

    добавлены Getters и отрисовка по условию

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,23 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- filter -->
+
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>Your have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -16,6 +30,7 @@
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 import TaskDetails from "./components/TaskDetails.vue";
 import { useTaskStore } from "./stores/TaskStore";
 
@@ -23,7 +38,10 @@ export default {
   components: { TaskDetails },
   setup() {
     const taskStore = useTaskStore();
-    return { taskStore };
+
+    const filter = ref("all");
+
+    return { taskStore, filter };
   },
 };
 </script>
